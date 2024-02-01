6a9f68f792d8360e681b276e690150ca30c8486a
kobmiz <kobimizrachi@icloud.com>
2024-02-01 21:01:13 +0200

    first commit

---
+++ howTo/script.js
@@ -0,0 +1,17 @@
+function welcomeMessage() {
+    alert('Welcome to my site');
+}
+function showmessage() {
+    console.log("ההודעה נשלחה בהצלחה");
+    /*.error / .warn */
+}
+function welcomeName() {
+    var name = document.getElementById("userName").value
+    alert("ברוך הבא " + name);
+
+}
+
+function multi50() {
+    var n = document.getElementById("yourNumber").value;
+    alert(n * 50)
+}
---
+++ lesson2/script.js
@@ -0,0 +1,36 @@
+let count = 0;
+
+function counter() {
+    count++;
+
+    document.getElementById("btn").innerHTML = count++;
+}
+
+function multi() {
+    const n1 = document.getElementById("num1").value;
+    const n2 = document.getElementById("num2").value;
+
+    /* alert(n1 * n2); */
+
+    document.getElementById("output1").innerHTML = n1 * n2;
+}
+
+function plus() {
+    const n3 = +document.getElementById("num3").value;
+    const n4 = +document.getElementById("num4").value;
+
+    document.getElementById("output2").innerHTML = n3 + n4;
+}
+
+function plusplus() {
+    const n5 = +document.getElementById("num5").value;
+    const n6 = +document.getElementById("num6").value;
+    const sum = n5 + n6;
+    /* const result = n5 + " + " + n6 + " = " + sum; */
+    const result = `${n5} + ${n6} = ${sum}`;
+    ``
+
+    document.getElementById("output3").innerHTML = n5 + n6;
+    document.getElementById("output3").innerHTML = result;
+
+}
