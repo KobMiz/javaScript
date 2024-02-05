f98ad6033e9a2634a23385aad823b010be644581
kobmiz <kobimizrachi@icloud.com>
2024-02-05 17:54:52 +0200

    05.02.2024

---
+++ lesson4/script.js
@@ -0,0 +1,12 @@
+let counter = 1;
+
+function plus() {
+    counter++;
+    document.getElementById("count").innerHTML = counter;
+}
+
+function minus() {
+    counter--;
+    document.getElementById("count").innerHTML = counter;
+
+}
